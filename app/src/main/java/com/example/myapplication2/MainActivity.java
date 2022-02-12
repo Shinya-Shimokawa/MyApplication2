b64b1f887df1867705836ef0d2e80f82db075410
HAO <bych0220@gmail.com>
2022-02-12 20:49:30 +0900

    a

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
